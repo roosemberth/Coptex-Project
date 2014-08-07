f8b1569d14125ee812f59bbb9c6c3e75269d8f52
Roosemberth Palacios <roosemberth@gmail.com>
2014-08-06 21:53:46 -0600

    Fixed Namespaces Issue.

--- SCP-Platform/SCP-src/SCP-HwCtrl/SCP-HwCtrl.cpp
+++ SCP-Platform/SCP-src/SCP-HwCtrl/SCP-HwCtrl.cpp
@@ -11,16 +11,17 @@
 #include "USB-Pololu-Maestro/Maestro-Definitions.h"
 #include "SCP-HwCtrl.h"
 
-namespace HwCtrl{
-	void C_MaestroUpdateQueue::EnqueuePositionUpdate(char Channel, unsigned short Position){
-		this->UpdateQueue.UpdatePending |= 1<<Channel;
-		this->UpdateQueue.Position[Channel-1] = Position;
-	}
-	unsigned short C_MaestroUpdateQueue::QueryPositionUpdate(char Channel){
-		return (this->UpdateQueue.UpdatePending & 1<<Channel)?this->UpdateQueue.Position[Channel-1]:0;
-	}
-	int C_MaestroUpdateQueue::FlushPendingUpdates(){
-		//TODO: Implement Maestro Comunication Platform
-		return 0;
-	}
+using namespace HwCtrl;
+
+/* Class C_MaestroUpdateQueue Public Functions Definitions */
+void C_MaestroUpdateQueue::EnqueuePositionUpdate(char Channel, unsigned short Position){
+	this->UpdateQueue.UpdatePending |= 1<<Channel;
+	this->UpdateQueue.Position[Channel-1] = Position;
+}
+unsigned short C_MaestroUpdateQueue::QueryPositionUpdate(char Channel){
+	return (this->UpdateQueue.UpdatePending & 1<<Channel)?this->UpdateQueue.Position[Channel-1]:0;
+}
+int C_MaestroUpdateQueue::FlushPendingUpdates(){
+	//TODO: Waiting Maestro Comunication Platform
+	return 0;
 }
